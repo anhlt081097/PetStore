0f2dbc37171dc19f568a6c7bec61f8d45cb9cb73
Anhlt <anhlt081097@gmail.com>
2022-11-03 19:52:06 +0700

    petstore

---
+++ src/main/java/com/example/PetStore/PetStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.PetStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PetStore/PetStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PetStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
